d8f7817bdc76a84cc2f0596762b1f8197d8e4571
Marei Morsy <MareiMorsy@github.com>
2018-10-13 00:22:50 +0200

    complex response

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 header("Content-Type: application/json;charset=utf-8");
 $debug = var_export($_REQUEST, true);
-echo "{\"fulfillmentText\": \"This is a text response\"}";
+echo "{\"fulfillmentText\": \"$debug\"}";
