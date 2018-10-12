edafdebc0c655544bd7021b4287198fe8fcce3f1
Marei Morsy <MareiMorsy@github.com>
2018-10-13 00:08:15 +0200

    fixing response

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
 <?php
+header("Content-Type: application/json;charset=utf-8");
 $debug = var_export($_REQUEST, true);
-echo "
+echo '
 {
-    'conversationToken': '[]',
-    'expectUserResponse': true,
-    'expectedInputs': [
+    "conversationToken": "[]",
+    "expectUserResponse": true,
+    "expectedInputs": [
       {
-        'inputPrompt': {
-          'richInitialPrompt': {
-            'items': [
+        "inputPrompt": {
+          "richInitialPrompt": {
+            "items": [
               {
-                'simpleResponse': {
-                  'textToSpeech': '$debug'
+                "simpleResponse": {
+                  "textToSpeech": "'.$debug.'"
                 }
               }
             ]
           }
         },
-        'possibleIntents': [
+        "possibleIntents": [
           {
-            'intent': 'assistant.intent.action.TEXT'
+            "intent": "assistant.intent.action.TEXT"
           }
         ],
-        'speechBiasingHints': [
-          '$songName'
+        "speechBiasingHints": [
+          "$songName"
         ]
       }
     ],
-    'responseMetadata': {
-      'status': {
-        'code': 14,
-        'message': 'Webhook error (206)'
+    "responseMetadata": {
+      "status": {
+        "code": 14,
+        "message": "Webhook error (206)"
       },
-      'queryMatchInfo': {
-        'queryMatched': true,
-        'intent': '4ed3647e-dfc9-4dd1-949e-45b582e7e890',
-        'parameterNames': [
-          'songName'
+      "queryMatchInfo": {
+        "queryMatched": true,
+        "intent": "4ed3647e-dfc9-4dd1-949e-45b582e7e890",
+        "parameterNames": [
+          "songName"
         ]
       }
     }
   }
-";
+';
