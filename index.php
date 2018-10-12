f466a66685b78c606e0e4dbe510840044214e79b
Marei Morsy <MareiMorsy@github.com>
2018-10-13 00:33:03 +0200

    complex response3

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 header("Content-Type: application/json;charset=utf-8");
-$debug = file_get_contents('php://input');
-echo "{\"fulfillmentText\": \"$debug\"}";
+echo "{\"fulfillmentText\": \"$HTTP_RAW_POST_DATA\"}";
