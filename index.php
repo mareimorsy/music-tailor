c5df33d750fa4f38bfdd1ce37cde68805d73cde8
Marei Morsy <MareiMorsy@github.com>
2018-10-13 09:51:19 +0200

    comment

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 header("Content-Type: application/json;charset=utf-8");
-// var_dump($GLOBA); 
-
-$data = file_get_contents('php://input');
-
-echo "{\"fulfillmentText\": \"playing...\"}";
-
-$my_file = 'file.txt';
-$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-fwrite($handle, $data);
+$req=json_decode(file_get_contents('php://input'), false);
+$songName = $req->queryResult->parameters->songName;
+echo "{\"fulfillmentText\": \"$songName is now playing\"}";
