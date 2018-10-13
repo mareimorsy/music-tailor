614dc0cec0e0479b92ebd00bd34698fb7665489d
Marei Morsy <MareiMorsy@github.com>
2018-10-13 11:38:08 +0200

    test ssml6

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header("Content-Type: application/json;charset=utf-8");
 $req=json_decode(file_get_contents('php://input'), false);
 $songName = $req->queryResult->parameters->songName;
-$speech = '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
 // echo "{\"fulfillmentText\": \"$speech\"}";
-$string = "<speak><audio src=\"https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg\">didn't get your MP3 audio file</audio></speak>";
-echo "{\"ssml\": \"$string\", \"displayText\": \"Hello\"}";
+$string = "<speak><audio src=\"https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg\">didnt get your MP3 audio file</audio></speak>";
+echo "{\"ssml\": \"$string\"}";
