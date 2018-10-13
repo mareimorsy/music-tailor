04070b5805087c1ee63db564f3625dc455dfc358
Marei Morsy <MareiMorsy@github.com>
2018-10-13 17:47:39 +0200

    test ssml28

--- index.php
+++ index.php
@@ -1,14 +1,47 @@
 <?php
-header("Content-Type: application/json;charset=utf-8");
+// header("Content-Type: application/json;charset=utf-8");
 $req=json_decode(file_get_contents('php://input'), false);
-$songName = $req->queryResult->parameters->songName;
+$songName  = $req->queryResult->parameters->songName;
+$queryText = $req->queryResult->queryText;
 // echo "{\"fulfillmentText\": \"$speech\"}";
 $string = "<speak><audio src='https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg'>didnt get your MP3 audio file</audio></speak>";
-// echo "{\"fulfillmentText\": \"$string\"}";
 
 $songURL="https://music-tailor.herokuapp.com/songs/believer.mp3";
 
-echo '{"ssml": "<speak>'.$songName.' is now playing <audio src=\"'.$songURL.'\">your wave file</audio></speak>"}';
+
+// echo getSongName("play shape of you song");
+// echo getSongName("play shape of you");
+// echo getSongName("play circus music");
+// echo getSongName("I'd like to listen to shape of you song");
+// echo getSongName("I would like to listen to shape of you song");
+// echo getSongName("I would like to listen to shape of you");
+// echo getSongName("I'd like to listen to shape of you");
+// echo getSongName("I'd like to listen to shape of you");
+
+
+function getSongName($queryText){
+    $removeList = ["play", "i'd", "i would", "like to", "listen to", "song", "music"];
+    $songName = strtolower($queryText);
+    foreach($removeList as $word){
+        $songName = str_replace($word, '', $songName);
+    }
+    return $songName;
+}
+
+
+switch ($songName) {
+    case "shape of you":
+        $songURL = "";
+        break;
+    case "blue":
+        $songURL = "";
+        break;
+    case "green":
+        $songURL = "";
+        break;
+    default:
+        $songURL = "";
+}
 
 
 
