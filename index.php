dafa02227f4a07fb7263788d03cb6b724f0adf9c
Marei Morsy <MareiMorsy@github.com>
2018-10-13 10:40:35 +0200

    test sound3

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ header("Content-Type: application/json;charset=utf-8");
 $req=json_decode(file_get_contents('php://input'), false);
 $songName = $req->queryResult->parameters->songName;
 $speech = '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-echo "{\"fulfillmentText\": \"$speech\"}";
+// echo "{\"fulfillmentText\": \"$speech\"}";
 
-// echo "{
-//     \"speech\": \"$speech\",
-//     \"displayText\": \"$speech\",
-//     \"source\": \"webhook-echo-sample\"
-// }";
+echo "{
+    \"speech\": \"$speech\",
+    \"displayText\": \"$speech\",
+    \"source\": \"webhook-echo-sample\"
+}";
